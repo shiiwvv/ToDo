72894a49da18a81bfa0730415a41d9aa4d74dee6
shiiwvv <shivkumar29xyz@gmail.com>
2026-05-23 00:03:23 +0530

    Added Features like Edit Content. Mark as Checked and unckecked, used specific colors to show the status of the task

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const form = document.querySelector('form');
 
+const saveButton = document.createElement('button');
+saveButton.classList.add('read-buttons' , 'save-edit');
+saveButton.innerText = "Save";
+
 const tasks =  JSON.parse(localStorage.getItem('tasks')) || [];
 const display = document.querySelector('.display-tasks');
+const windoww = document.querySelector('.show-task');
+const main = document.querySelector('.main-container');
+const clearTask = document.querySelector('.Clear-Tasks');
 displayTask();
+
+clearTask.addEventListener('click' , () => {
+    console.log("Local Storage Cleared.");
+    localStorage.clear();
+});
 
 display.addEventListener('click' , deleteTask);
 display.addEventListener('click' , readMore)
@@ -24,7 +36,6 @@ function addToLocal(input){
     const task = createTaskObject(input);
     tasks.push(task);
     localStorage.setItem('tasks' , JSON.stringify(tasks));
-    console.log("Pushed to local");
     displayTask();
 }
 
@@ -36,19 +47,21 @@ function createTaskObject(input){
         "time" : date.toLocaleTimeString(),
         "state": false, 
     }
-
     return task;
 }
 
 function displayTask(){
     display.innerHTML = "";
     tasks.forEach((element , index) => {
-
         const div = document.createElement('div');
         div.classList.add('task');
+        setBorder(element , div);
+        const taskStatement = element.task.substring(0 , 200);
         div.dataset.Id = `${index}`;
         div.innerHTML = `
-            <p>${element.task}</p>
+            <div class = "task-text-container">
+                <p>${taskStatement}</p>
+            </div>
             <div class="buttons">
                 <button class="open-task butt">Read</button>
                 <button class="Del-task butt">Delete</button>
@@ -57,7 +70,6 @@ function displayTask(){
         display.appendChild(div);
     });
 }
-
 
 
 function deleteTask(event){
@@ -82,7 +94,102 @@ function readMore(event){
         console.log("Open segment");
         
         const task = event.target.closest('.task');
+        const taskId = Number(task.dataset.Id);
+        const taskObj = tasks[taskId];
+        console.log(typeof taskId);
+
+        // windoww.style.display = "flex";
+        windoww.classList.add('open');
+        main.classList.add("opac");
+        setBorder(taskObj , windoww);
+
+        windoww.innerHTML = `
+            <div class = "Inside-Open" data-Id = "${taskId}">
+                <div class = "window-head">
+                    <div class = "display-time-and-date">
+                        <h2>Task ${taskId + 1}</h2>
+                        <div class = "display-Date">
+                            <p>${taskObj.date}</p>
+                            <p>${taskObj.time}</p>
+                        </div>
+                    </div>
+                    <span class="Close">Close</span>
+                </div>
+
+                <div class = "window-content" contenteditable="true">
+                        <p class="text-para">${taskObj.task}</p>
+                </div>
+                <div class = "edit-buttons">
+                    <button class = "checked read-buttons">Not Completed</button>
+                </div>
+            </div>
+        `;
     }
 }
+
+function setBorder(element , div){
+        if(element.state == false){
+            div.style.border = `1px solid red`;
+        }
+        else{
+            div.style.border = `1px solid green`;
+        }
+}
+
+windoww.addEventListener('click' , (event) => {
+    event.preventDefault();
+
+    const targetEvent = event.target;
+    const closest = targetEvent.closest(".Inside-Open");
+    const targetId = Number(closest.dataset.id);
+    
+
+    //CLosing the Window
+    if(targetEvent.classList.contains('Close')){
+        windoww.classList.remove('open');
+        main.classList.remove("opac");
+    }
+
+    //Allowing Edit
+    if(targetEvent.classList.contains('text-para') || targetEvent.classList.contains('window-content')){
+        const addButton = document.querySelector('.edit-buttons');
+        addButton.appendChild(saveButton);
+    }
+
+    //Saving Edit
+    if(targetEvent.classList.contains('save-edit') ){
+        const input = document.querySelector('.text-para').textContent;
+
+        const taskObj = JSON.parse(localStorage.getItem('tasks'));
+        
+        taskObj[targetId].task = input;
+
+        localStorage.setItem('tasks' , JSON.stringify(taskObj));
+
+        displayTask();
+    }
+    
+    if(targetEvent.classList.contains('checked')){
+        const taskObj = JSON.parse(localStorage.getItem('tasks'));
+        
+        const checkButton = document.querySelector('.checked');
+
+        if(checkButton.innerText == "Not Completed"){
+            checkButton.innerText = "Completed";
+            checkButton.style.background = "green";
+            taskObj[targetId].state = true;
+            setBorder(taskObj[targetId] , windoww); 
+        }else{
+            checkButton.innerText = "Not Completed";
+            checkButton.style.background = "red";
+            taskObj[targetId].state = false;
+            setBorder(taskObj[targetId] , windoww); 
+        }
+    
+        localStorage.setItem('tasks' , JSON.stringify(taskObj));
+    }
+})
+
+
 
 
